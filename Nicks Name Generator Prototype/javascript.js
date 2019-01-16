ca8f7c01880ba4ea6721d65fd1ba10ee950c0fa2
Jake Bentz <30503929+jakebentz@users.noreply.github.com>
2019-01-16 00:27:43 -0600

    Add files via upload

--- Nicks Name Generator Prototype/javascript.js
+++ Nicks Name Generator Prototype/javascript.js
@@ -23,9 +23,6 @@ var newLast = [
 function newName() {
   var randomNumber1 = Math.floor(Math.random() * (newFirst.length));
   var randomNumber2 = Math.floor(Math.random() * (newLast.length));
-  //document.getElementById('oldName').innerHTML = fname.value+ " "+ lname.value;
-//  var randomNumber1 = Math.floor(Math.random() * (newFirst.length));
-//  var randomNumber2 = Math.floor(Math.random() * (newLast.length));
   document.getElementById('oldName').innerHTML = "oldname: "+fname.value+" "+lname.value;
   document.getElementById('nameDisplay').innerHTML = "newname:" +newFirst[randomNumber1]+" "+newLast[randomNumber2];
 }
