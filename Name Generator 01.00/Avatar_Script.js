75b0251e0e5d8d6f9602b1ab0b49ec9e55660a22
Jake Bentz <30503929+jakebentz@users.noreply.github.com>
2019-01-17 23:40:45 -0600

    Add files via upload

--- Name Generator 01.00/Avatar_Script.js
+++ Name Generator 01.00/Avatar_Script.js
@@ -1,181 +1,698 @@
 var maleSeriousDodarms = [
-  'eater',
-  'drinker',
-  'gulper',
-  'chugger',
-  'masher',
-  'milker',
-  'crusher',
-  'dunker'
+'Liam',
+'Noah',
+'William',
+'James',
+'Logan',
+'Benjamin',
+'Mason',
+'Elijah',
+'Oliver',
+'Jacob',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
+'Nolan',
+'Nicholas',
+'Ezra',
+'Colton',
+'Angel',
+'Brayden',
+'Jordan',
+'Dominic',
+'Austin',
+'Benjamin',
+'Mason',
+'Elijah',
+'Oliver',
+'Jacob',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
+'Nolan',
+
 ]
 
 var maleSillyDodarms = [
-  'hotdog',
-  'appendage',
-  'whale',
-  'burp',
-  'thing',
-  'moof',
-  'waste',
-  'dill'
+'Lucas',
+'Michael',
+'Alexander',
+'Ethan',
+'Daniel',
+'Matthew',
+'Aiden',
+'Henry',
+'Joseph',
+'Jackson',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
+'Nolan',
+'Nicholas',
+'Ezra',
+'Colton',
+'Angel',
+'Brayden',
+'Jordan',
+'Dominic',
+'Austin',
+'Lucas',
+'Michael',
+'Alexander',
+'Ethan',
+'Daniel',
+'Matthew',
+'Aiden',
+'Henry',
+'Joseph',
+'Jackson',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
 ]
 
 var femaleSeriousDodarms = [
-  'eater',
-  'drinker',
-  'gulper',
-  'chugger',
-  'masher',
-  'milker',
-  'crusher',
-  'dunker'
+'Emma',
+'Olivia',
+'Ava',
+'Isabella',
+'Sophia',
+'Mia',
+'Charlotte',
+'Amelia',
+'Evelyn',
+'Abigail',
+'Harper',
+'Emily',
+'Elizabeth',
+'Avery',
+'Sofia',
+'Emma',
+'Olivia',
+'Ava',
+'Isabella',
+'Sophia',
+'Mia',
+'Charlotte',
+'Amelia',
+'Evelyn',
+'Abigail',
+'Emma',
+'Olivia',
+'Ava',
+'Isabella',
+'Sophia',
+'Mia',
+'Charlotte',
+'Amelia',
+'Evelyn',
+'Abigail',
+'Harper',
+'Emily',
+'Elizabeth',
+'Avery',
+'Sofia',
 ]
 
 var femaleSillyDodarms = [
-  'hotdog',
-  'appendage',
-  'whale',
-  'burp',
-  'thing',
-  'moof',
-  'waste',
-  'dill'
+'Ella',
+'Madison',
+'Scarlett',
+'Victoria',
+'Aria',
+'Grace',
+'Chloe',
+'Camila',
+'Penelope',
+'Riley',
+'Layla',
+'Lillian',
+'Nora',
+'Zoey',
+'Mila',
+'Ella',
+'Madison',
+'Scarlett',
+'Victoria',
+'Aria',
+'Grace',
+'Chloe',
+'Camila',
+'Penelope',
+'Riley',
+'Ella',
+'Madison',
+'Scarlett',
+'Victoria',
+'Aria',
+'Grace',
+'Chloe',
+'Camila',
+'Penelope',
+'Riley',
+'Layla',
+'Lillian',
+'Nora',
+'Zoey',
+'Mila',
 ]
 
 var maleSeriousWarensemble = [
-  'eater',
-  'drinker',
-  'gulper',
-  'chugger',
-  'masher',
-  'milker',
-  'crusher',
-  'dunker'
+'Samuel',
+'Sebastian',
+'David',
+'Carter',
+'Wyatt',
+'Jayden',
+'John',
+'Owen',
+'Dylan',
+'Luke',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
+'Nolan',
+'Nicholas',
+'Ezra',
+'Colton',
+'Angel',
+'Brayden',
+'Jordan',
+'Dominic',
+'Austin',
+'Samuel',
+'Sebastian',
+'David',
+'Carter',
+'Wyatt',
+'Jayden',
+'John',
+'Owen',
+'Dylan',
+'Luke',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
 ]
 
 var maleSillyWarensemble = [
-  'hotdog',
-  'appendage',
-  'whale',
-  'burp',
-  'thing',
-  'moof',
-  'waste',
-  'dill'
+'Gabriel',
+'Anthony',
+'Isaac',
+'Grayson',
+'Jack',
+'Julian',
+'Levi',
+'Christopher',
+'Joshua',
+'Andrew',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
+'Nolan',
+'Nicholas',
+'Ezra',
+'Colton',
+'Angel',
+'Brayden',
+'Jordan',
+'Dominic',
+'Austin',
+'Gabriel',
+'Anthony',
+'Isaac',
+'Grayson',
+'Jack',
+'Julian',
+'Levi',
+'Christopher',
+'Joshua',
+'Andrew',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
+'Nolan',
+'Nicholas',
+'Ezra',
+'Colton',
+'Angel',
+'Brayden',
 ]
 
 var femaleSeriousWarensemble = [
-  'eater',
-  'drinker',
-  'gulper',
-  'chugger',
-  'masher',
-  'milker',
-  'crusher',
-  'dunker'
+'Aubrey',
+'Hannah',
+'Lily',
+'Addison',
+'Eleanor',
+'Natalie',
+'Luna',
+'Savannah',
+'Brooklyn',
+'Leah',
+'Zoe',
+'Stella',
+'Hazel',
+'Ellie',
+'Paisley',
+'Aubrey',
+'Hannah',
+'Lily',
+'Addison',
+'Eleanor',
+'Natalie',
+'Luna',
+'Savannah',
+'Brooklyn',
+'Leah',
+'Aubrey',
+'Hannah',
+'Lily',
+'Addison',
+'Eleanor',
+'Natalie',
+'Luna',
+'Savannah',
+'Brooklyn',
+'Leah',
+'Zoe',
+'Stella',
+'Hazel',
+'Ellie',
+'Paisley',
+'Aubrey',
+'Hannah',
+'Lily',
 ]
 
 var femaleSillyWarensemble = [
-  'hotdog',
-  'appendage',
-  'whale',
-  'burp',
-  'thing',
-  'moof',
-  'waste',
-  'dill'
+'Audrey',
+'Skylar',
+'Violet',
+'Claire',
+'Bella',
+'Aurora',
+'Lucy',
+'Anna',
+'Samantha',
+'Caroline',
+'Genesis',
+'Aaliyah',
+'Kennedy',
+'Kinsley',
+'Allison',
+'Audrey',
+'Skylar',
+'Violet',
+'Claire',
+'Bella',
+'Aurora',
+'Lucy',
+'Anna',
+'Samantha',
+'Caroline',
+'Audrey',
+'Skylar',
+'Violet',
+'Claire',
+'Bella',
+'Aurora',
+'Lucy',
+'Anna',
+'Samantha',
+'Caroline',
+'Genesis',
+'Aaliyah',
+'Kennedy',
+'Kinsley',
 ]
 
 var maleSeriousCandle = [
-  'eater',
-  'drinker',
-  'gulper',
-  'chugger',
-  'masher',
-  'milker',
-  'crusher',
-  'dunker'
+'Lincoln',
+'Mateo',
+'Ryan',
+'Jaxon',
+'Nathan',
+'Aaron',
+'Isaiah',
+'Thomas',
+'Charles',
+'Caleb',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
+'Nolan',
+'Nicholas',
+'Ezra',
+'Colton',
+'Angel',
+'Brayden',
+'Jordan',
+'Dominic',
+'Austin',
+'Lincoln',
+'Mateo',
+'Ryan',
+'Jaxon',
+'Nathan',
+'Aaron',
+'Isaiah',
+'Thomas',
+'Charles',
+'Caleb',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
 ]
 
 var maleSillyCandle = [
-  'hotdog',
-  'appendage',
-  'whale',
-  'burp',
-  'thing',
-  'moof',
-  'waste',
-  'dill'
+'Josiah',
+'Christian',
+'Hunter',
+'Eli',
+'Jonathan',
+'Connor',
+'Landon',
+'Adrian',
+'Asher',
+'Cameron',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
+'Nolan',
+'Nicholas',
+'Ezra',
+'Colton',
+'Angel',
+'Brayden',
+'Jordan',
+'Dominic',
+'Austin',
+'Josiah',
+'Christian',
+'Hunter',
+'Eli',
+'Jonathan',
+'Connor',
+'Landon',
+'Adrian',
+'Asher',
+'Cameron',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
 ]
 
 var femaleSeriousCandle = [
-  'eater',
-  'drinker',
-  'gulper',
-  'chugger',
-  'masher',
-  'milker',
-  'crusher',
-  'dunker'
+'Maya',
+'Sarah',
+'Madelyn',
+'Adeline',
+'Alexa',
+'Ariana',
+'Elena',
+'Gabriella',
+'Naomi',
+'Alice',
+'Gianna',
+'Isabelle',
+'Julia',
+'Valentina',
+'Nova',
+'Clara',
+'Vivian',
+'Reagan',
+'Mackenzie',
+'Madeline',
+'Quinn',
+'Maya',
+'Sarah',
+'Madelyn',
+'Adeline',
+'Alexa',
+'Ariana',
+'Elena',
+'Gabriella',
+'Naomi',
+'Alice',
+'Gianna',
+'Isabelle',
+'Julia',
+'Valentina',
+'Nova',
+'Clara',
 ]
 
 var femaleSillyCandle = [
-  'hotdog',
-  'appendage',
-  'whale',
-  'burp',
-  'thing',
-  'moof',
-  'waste',
-  'dill'
+'Maya',
+'Sarah',
+'Madelyn',
+'Adeline',
+'Alexa',
+'Ariana',
+'Elena',
+'Gabriella',
+'Naomi',
+'Alice',
+'Sadie',
+'Hailey',
+'Eva',
+'Emilia',
+'Autumn',
+'Gianna',
+'Isabelle',
+'Julia',
+'Valentina',
+'Nova',
+'Clara',
+'Vivian',
+'Reagan',
+'Mackenzie',
+'Madeline',
+'Quinn',
+'Maya',
+'Sarah',
+'Madelyn',
+'Adeline',
+'Alexa',
+'Ariana',
+'Elena',
+'Gabriella',
+'Naomi',
+'Alice',
+'Sadie',
+'Hailey',
+'Eva',
+'Emilia',
+'Autumn',
+'Gianna',
 ]
 
 var maleSeriousMilk = [
-  'eater',
-  'drinker',
-  'gulper',
-  'chugger',
-  'masher',
-  'milker',
-  'crusher',
-  'dunker'
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
+'Nolan',
+'Nicholas',
+'Ezra',
+'Colton',
+'Angel',
+'Brayden',
+'Jordan',
+'Dominic',
+'Austin',
+'Leo',
+'Theodore',
+'Jeremiah',
+'Hudson',
+'Robert',
+'Easton',
+'Nolan',
+'Nicholas',
+'Ezra',
+'Colton',
+'Angel',
+'Brayden',
+'Jordan',
+'Dominic',
 ]
 
 var maleSillyMilk = [
-  'hotdog',
-  'appendage',
-  'whale',
-  'burp',
-  'thing',
-  'moof',
-  'waste',
-  'dill'
+  'John',
+  'Paul',
+  'George',
+  'David',
+  'Frank',
+  'Ben',
+  'Steve',
+  'Bill',
+  'Andy',
+  'Buzz',
+  'Leo',
+  'Theodore',
+  'Jeremiah',
+  'Hudson',
+  'Robert',
+  'Easton',
+  'Nolan',
+  'Nicholas',
+  'Ezra',
+  'Colton',
+  'Angel',
+  'Brayden',
+  'Jordan',
+  'Dominic',
+  'Austin',
+  'John',
+  'Paul',
+  'George',
+  'David',
+  'Frank',
+  'Ben',
+  'Steve',
+  'Bill',
+  'Andy',
+  'Buzz',
+  'Leo',
+  'Theodore',
+  'Jeremiah',
+  'Hudson',
+  'Robert',
+  'Easton',
 ]
 
 var femaleSeriousMilk = [
-  'eater',
-  'drinker',
-  'gulper',
-  'chugger',
-  'masher',
-  'milker',
-  'crusher',
-  'dunker'
+'Quinn',
+'Nevaeh',
+'Piper',
+'Ruby',
+'Serenity',
+'Willow',
+'Everly',
+'Cora',
+'Kaylee',
+'Lydia',
+'Aubree',
+'Arianna',
+'Eliana',
+'Peyton',
+'Melanie',
+'Gianna',
+'Isabelle',
+'Julia',
+'Valentina',
+'Nova',
+'Clara',
+'Vivian',
+'Reagan',
+'Mackenzie',
+'Madeline',
+'Quinn',
+'Quinn',
+'Nevaeh',
+'Piper',
+'Ruby',
+'Serenity',
+'Willow',
+'Everly',
+'Cora',
+'Kaylee',
+'Lydia',
+'Aubree',
+'Arianna',
+'Eliana',
+'Peyton',
+'Melanie',
+'Gianna',
+'Isabelle',
+'Julia',
+'Valentina',
 ]
 
 var femaleSillyMilk = [
-  'hotdog',
-  'appendage',
-  'whale',
-  'burp',
-  'thing',
-  'moof',
-  'waste',
-  'dill'
+'Gianna',
+'Isabelle',
+'Julia',
+'Valentina',
+'Nova',
+'Clara',
+'Vivian',
+'Reagan',
+'Mackenzie',
+'Madeline',
+'Quinn',
+'Nevaeh',
+'Piper',
+'Ruby',
+'Serenity',
+'Gianna',
+'Isabelle',
+'Julia',
+'Valentina',
+'Nova',
+'Clara',
+'Vivian',
+'Reagan',
+'Mackenzie',
+'Madeline',
+'Quinn',
+'Gianna',
+'Isabelle',
+'Julia',
+'Valentina',
+'Nova',
+'Clara',
+'Vivian',
+'Reagan',
+'Mackenzie',
+'Madeline',
+'Quinn',
+'Nevaeh',
+'Piper',
+'Ruby',
+'Serenity',
+'Gianna',
+'Isabelle',
 ]
 
 function newName() {
-var newName = maleSillyDodarms;
+//var newName = maleSillyDodarms;
+//var customNumber = 1;
 var vGender = document.getElementById('gender').value;
 var vFloor = document.getElementById('floor').value;
 var vFaction = document.getElementById('faction').value;
@@ -184,23 +701,22 @@ var numGender = document.getElementById('gender').value.length;
 var numFloor = document.getElementById('floor').value.length;
 var numFaction = document.getElementById('faction').value.length;
 var numTone = document.getElementById('tone').value.length;
-var result = "maleSeriousDodarms";
-var customNumber = 1
+//var result = "maleSeriousDodarms";
 
 //determine gender, tone, and faction
 //maleSerious
 if(vGender == "male" && vTone == "serious"){
   if(vFaction == "dodarms"){
-    result = "maleSeriousDodarms"
+    result = maleSeriousDodarms
   } else {
     if(vFaction == "warensemble"){
-      result = "maleSeriousWarensemble"
+      result = maleSeriousWarensemble
     } else {
       if(vFaction == "candle"){
-        result = "maleSeriousCandle"
+        result = maleSeriousCandle
       } else {
         if(vFaction == "milk"){
-          result = "maleSeriousMilk"
+          result = maleSeriousMilk
         }
       }
     }
@@ -209,16 +725,16 @@ if(vGender == "male" && vTone == "serious"){
 //femaleSerious
 if(vGender == "female" && vTone == "serious"){
   if(vFaction == "dodarms"){
-    result = "femaleSeriousDodarms"
+    result = femaleSeriousDodarms
   } else {
     if(vFaction == "warensemble"){
-      result = "femaleSeriousWarensemble"
+      result = femaleSeriousWarensemble
     } else {
       if(vFaction == "candle"){
-        result = "femaleSeriousCandle"
+        result = femaleSeriousCandle
       } else {
         if(vFaction == "milk"){
-          result = "femaleSeriousMilk"
+          result = femaleSeriousMilk
         }
       }
     }
@@ -227,16 +743,16 @@ if(vGender == "female" && vTone == "serious"){
 //maleSilly
 if(vGender == "male" && vTone == "silly"){
   if(vFaction == "dodarms"){
-    result = "maleSillyDodarms"
+    result = maleSillyDodarms
   } else {
     if(vFaction == "warensemble"){
-      result = "maleSillyWarensemble"
+      result = maleSillyWarensemble
     } else {
       if(vFaction == "candle"){
-        result = "maleSillyCandle"
+        result = maleSillyCandle
       } else {
         if(vFaction == "milk"){
-          result = "maleSillyMilk"
+          result = maleSillyMilk
         }
       }
     }
@@ -245,32 +761,61 @@ if(vGender == "male" && vTone == "silly"){
 //femaleSilly
 if(vGender == "female" && vTone == "silly"){
   if(vFaction == "dodarms"){
-    result = "femaleSillyDodarms"
+    result = femaleSillyDodarms
   } else {
     if(vFaction == "warensemble"){
-      result = "femaleSillyWarensemble"
+      result = femaleSillyWarensemble
     } else {
       if(vFaction == "candle"){
-        result = "femaleSillyCandle"
+        result = femaleSillyCandle
       } else {
         if(vFaction == "milk"){
-          result = "femaleSillyMilk"
+          result = femaleSillyMilk
         }
       }
     }
   }
 }
 
-//Determine number
-number = numTone+numFloor+numGender+numFloor+numFaction
+//Determine the name
+var newName = result
+var number = (numTone+numFloor+numGender+numFloor+numFaction)-10
+var nameName = result[number]
+
+//Determine tone
+if (vTone == "serious"){
+nameTone = "Tough";
+} else {
+  nameTone = "Silly";
+}
+
+//Determine faction
+var nameFaction;
+if(vFaction == "dodarms"){
+  nameFaction = "D.O.D. Arms";
+} else {
+  if(vFaction == "warensemble"){
+    nameFaction = "War Ensemble";
+  } else {
+    if(vFaction == "candle"){
+      nameFaction = "Candle Wolf";
+    } else {
+      if(vFaction == "milk"){
+        nameFaction = "M.I.L.K";
+      }
+    }
+  }
+}
 
 //  var randomNumber1 = Math.floor(Math.random() * (newFirst.length));
 //  var randomNumber2 = Math.floor(Math.random() * (newLast.length));
 //  document.getElementById('oldName').innerHTML = "oldname: "+fname.value+" "+lname.value;
 //  document.getElementById('nameDisplay').innerHTML = "newname:" +newFirst[randomNumber1]+" "+newLast[randomNumber2];
-  document.getElementById('choices').innerHTML = "Choices: "+vGender+" "+vFloor+" "+vFaction+" "+vTone;
 //  document.getElementById('choices').innerHTML = "Variables: "+vGender+" "+vFloor" "+vFaction+" "+vTone;
-  document.getElementById('nameDisplay').innerHTML = "Avatar Name:" +newName[customNumber];
-  document.getElementById('result').innerHTML = "Result: " + result + " Number: "+number;
+//  document.getElementById('nameDisplay').innerHTML = "Avatar Name: " +result[number];
+  document.getElementById('nameDisplay').innerHTML = "Avatar Name: " + nameTone + " " + nameFaction +" "+nameName;
+//  document.getElementById('result').innerHTML = "Result: " + result + " Number: "+number;
+//  document.getElementById('choices').innerHTML = "Choices: "+vGender+" "+vFloor+" "+vFaction+" "+vTone;
+
   //
 }
